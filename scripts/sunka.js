ddd999484c9834ed42300e66bf445655d7f3fa97
Dave Taylor <dave@the-taylors.org>
2011-07-14 18:05:16 +0100

    added players and pots

--- scripts/sunka.js
+++ scripts/sunka.js
@@ -2,16 +2,63 @@
     sunka game
 -------------------------------------------------------*/
 define(function(){
-    var currentPlayer       = 1;
+    var currentPlayer       = 1,
+        players             = [],
+        pots                = [];
+
+    var Pot = function() {
+        
+    };
+    Pot.prototype = {
+        shellCount: 7
+    };
+
+
+    var Player = function(){
+    };
+    Player.prototype = {
+        pot: new Pot()
+    };
 
     var exports = {
+        allPots: function() {
+            return pots;
+        },
+        // player
         currentPlayer: function(){
             return currentPlayer;
         },
+        players: function() {
+            return players;
+        },
+        potCount: function(player) {
+            return players[player-1].pot.shellCount;
+        },
         startGame: function(){
+            players = [
+                new Player(),
+                new Player()
+            ];
+            pots = [];
+            pots.unshift(new Pot(pots[1]));
+            pots.unshift(new Pot(pots[1]));
+            pots.unshift(new Pot(pots[1]));
+            pots.unshift(new Pot(pots[1]));
+            pots.unshift(new Pot(pots[1]));
+            pots.unshift(new Pot(pots[1]));
+            pots.unshift(new Pot(pots[1]));
+            pots.unshift(new Pot(pots[1]));
+            pots.unshift(new Pot(pots[1]));
+            pots.unshift(new Pot(pots[1]));
+            pots.unshift(new Pot(pots[1]));
+            pots.unshift(new Pot(pots[1]));
+            pots.unshift(new Pot(pots[1]));
+            pots.unshift(new Pot(pots[1]));
             return true;
         },
-        turn: function(pot) {
+        turn: function(potNumber) {
+            var pot = pots[potNumber - 1];
+            // pot.empty();
             return true;
         }
     };
--- tests/spec/sunka.js
+++ tests/spec/sunka.js
@@ -22,11 +22,19 @@ describe("Sunka", function() {
 
         it('can start a new game', function(){
             expect(sunka.startGame()).toBeTruthy();
+        });
+        it('and have players set up', function(){
+            expect(sunka.players().length).toEqual(2);
             expect(sunka.currentPlayer()).toEqual(1);
+        });
+        it('and have pots set up', function(){
+            expect(sunka.allPots().length).toEqual(14);
+            expect(sunka.allPots()[6].nextPot).toBe(null);
         });
 
         it('can take a turn', function(){
             expect(sunka.turn(1)).toBeTruthy();
+            expect(sunka.potCount(1)).toEqual(1);
         });
         
     });
